ea67e71912e4cca09c578cee5f5fd64becf95466
Uncurlhalo <jmelton116@gmail.com>
2024-09-19 20:12:32 -0400

    add: update .gitignore, remove output files, add ansible inventory templating

---
+++ terraform/ansible.tf
@@ -0,0 +1,13 @@
+locals {
+  depends_on    = [module.k8s-control-plane, module.k8s-workers-large, module.k8s-workers-medium, module.k8s-workers-small]
+  control_nodes = module.k8s-control-plane.node_info
+  worker_nodes  = concat(module.k8s-workers-large.node_info, module.k8s-workers-medium.node_info, module.k8s-workers-small.node_info)
+}
+
+resource "local_file" "ansible_inventory" {
+  filename = "../ansible//inventory/inventory.ini"
+  content = templatefile("./templates/inventory.ini.tftpl", {
+    control_nodes = join("\n", [for host in local.control_nodes : join(" ", [host.hostname, "ansible_host_ssh=${host.ip}", "ansible_connection=ssh"])])
+    worker_nodes  = join("\n", [for host in local.worker_nodes : join(" ", [host.hostname, "ansible_host_ssh=${host.ip}", "ansible_connection=ssh"])])
+  })
+}
--- terraform/modules/k8s-vm-node/output.tf
+++ terraform/modules/k8s-vm-node/output.tf
@@ -1,10 +1,9 @@
-output "node_ipv4_address" {
+output "node_info" {
   depends_on = [proxmox_virtual_environment_vm.k8s-node]
-  value      = proxmox_virtual_environment_vm.k8s-node[*].ipv4_addresses[1][0]
-}
-
-resource "local_file" "node_ips" {
-  content         = join("\n", proxmox_virtual_environment_vm.k8s-node[*].ipv4_addresses[1][0])
-  filename        = "output/k8s-${var.k8s_node_spec.type}-${var.k8s_node_spec.name}-ips.txt"
-  file_permission = "0644"
+  value = [
+    for host in proxmox_virtual_environment_vm.k8s-node : {
+      "hostname" : host.name
+      "ip" : host.ipv4_addresses[1][0]
+    }
+  ]
 }
