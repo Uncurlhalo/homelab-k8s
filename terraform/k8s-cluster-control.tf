1122cba9bd6140aa7284cc4af2b9a2b8d751972d
Uncurlhalo <jmelton116@gmail.com>
2024-10-01 18:02:55 -0400

    change: rename some files, use some more sane names and variables

--- terraform/k8s-cluster-control.tf
+++ terraform/k8s-cluster-control.tf
@@ -1,7 +1,7 @@
 # Use modules to reference our control plane
-module "k8s-control-plane" {
+module "k8s-cluster-control" {
   # depend on the config file creations
-  depends_on = [proxmox_virtual_environment_download_file.ubuntu_cloud_img, proxmox_virtual_environment_file.cloud-init]
+  depends_on = [proxmox_virtual_environment_download_file.ubuntu_cloud_img, proxmox_virtual_environment_file.cloud-init-k8s]
   # reference local module
   source = "./modules/k8s-node"
   providers = {
@@ -10,7 +10,7 @@ module "k8s-control-plane" {
 
   # Define our variables
   k8s_node_spec = {
-    name         = "control-plane"
+    name         = "control-node"
     count        = var.control_node_count
     cores        = 4
     memory       = 8192
@@ -19,12 +19,11 @@ module "k8s-control-plane" {
   }
 
   vm_image_id   = proxmox_virtual_environment_download_file.ubuntu_cloud_img.id
-  cloud_init_id = proxmox_virtual_environment_file.cloud-init.id
+  cloud_init_id = proxmox_virtual_environment_file.cloud-init-k8s.id
 
   node_name = var.pve.node_name
   vm_dns    = var.vm_dns
   zfs_disk  = false
 
-  vm_private_ip_prefix = "1"
-  vm_public_ip_prefix  = "21"
+  vm_public_ip_prefix = "21"
 }
--- terraform/k8s-cluster-worker.tf
+++ terraform/k8s-cluster-worker.tf
@@ -1,7 +1,7 @@
 # Use modules to reference our 3 worker cluster sizes
-module "k8s-workers" {
+module "k8s-cluster-worker" {
   # depned on control nodes completed and large completed
-  depends_on = [module.k8s-control-plane]
+  depends_on = [module.k8s-cluster-control]
   # reference local module
   source = "./modules/k8s-node"
   providers = {
@@ -19,12 +19,11 @@ module "k8s-workers" {
   }
 
   vm_image_id   = proxmox_virtual_environment_download_file.ubuntu_cloud_img.id
-  cloud_init_id = proxmox_virtual_environment_file.cloud-init.id
+  cloud_init_id = proxmox_virtual_environment_file.cloud-init-k8s.id
 
   node_name = var.pve.node_name
   vm_dns    = var.vm_dns
   zfs_disk  = true
 
-  vm_private_ip_prefix = "2"
-  vm_public_ip_prefix  = "22"
+  vm_public_ip_prefix = "22"
 }
