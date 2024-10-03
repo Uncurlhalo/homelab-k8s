7bd5e7a00d40ce93f02253fb6c9a2fcfeac9bb8d
Uncurlhalo <jmelton116@gmail.com>
2024-10-02 22:41:26 -0400

    add: re-add the core ansible files you need to run the playbook

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -4,7 +4,7 @@ locals {
 }
 
 resource "local_file" "ansible_inventory" {
-  filename = "../ansible/inventory/inventory.ini"
+  filename = "../kubespray/inventory/homelab-k8s/inventory.ini"
   content = templatefile("./templates/inventory.ini.tftpl", {
     control_nodes       = join("\n", [for host in local.control_nodes : join(" ", [host.hostname, "ansible_host=${host.ip_pub}", "ansible_user=${var.vm_user}", "ip=${host.ip_pub}"])])
     worker_nodes        = join("\n", [for host in local.worker_nodes : join(" ", [host.hostname, "ansible_host=${host.ip_pub}", "ansible_user=${var.vm_user}", "ip=${host.ip_pub}"])])
